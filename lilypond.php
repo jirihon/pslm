8c3ae45d6a85e398a4d26c01cfce01e1645e06bb
Jiri Hon <jiri.hon@gmail.com>
2022-01-26 11:34:59 +0100

    typo

--- lilypond.php
+++ lilypond.php
@@ -8,15 +8,13 @@ if (preg_match('#\.pslm$#', $pslm_f)) {
     // convert .pslm to .ly before running lilypond
     $psalm = file_get_contents($pslm_f);
     $psalm = pslm_parse_psalm($psalm);
-    $lily = pslm_lilypond($psalm, 7, 100);
+    $lily = pslm_lilypond($psalm, 10, 100);
 
     $lily_f = preg_replace('#\.pslm$#', '.ly', $pslm_f);
     file_put_contents($lily_f, $lily);
 
     $passed_args = array_slice($argv, 1, -1);
     $passed_args[] = $lily_f;
-
-    $cmd = "lilypond  $passed_args $lily_f";
 } else {
     // fallback to basic lilypond
     $passed_args = array_slice($argv, 1);
