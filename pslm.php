551e870588fc6e3be8ca859890034d085f322add
Jiří Hon <jiri.hon@gmail.com>
2022-02-25 09:06:53 +0100

    generalized hyphenantion exceptions

--- pslm.php
+++ pslm.php
@@ -558,8 +558,8 @@ function pslm_text_to_lyrics($text) {
     $htext = preg_replace($PSLM_HYPH_EXCEPTIONS[0], $PSLM_HYPH_EXCEPTIONS[1], $htext);
     $repl = [
         '#\s{2,}#' => ' ', // normalize white-spaces to single space
-        '# -- ([sšjdbr]) -- #ui' => '\1 -- ', // move s, š or j to the previous syllable if both options are possible
-        '# -- ([tzvn]|st) -- #ui' => ' -- \1', // move t, z, v, n to the next syllable if both options are posible
+        '# -- ([bdjlrřsš]) -- #ui' => '\1 -- ', // move some ambiguous consonants to the previous syllable if both options are possible
+        '# -- ([cčfghkmnňpqtťvwxyzžhcktzvnk]|st) -- #ui' => ' -- \1', // move other ambiguous consonants to the next syllable if both options are posible
         '# -- (sť|ls|ch)\b#ui' => '\1', // move unsyllabic parts to the previous syllable
         
         '#\bjs -- me\b#ui' => 'jsme', // fix js -- me
