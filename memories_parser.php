a33fc6c2ad61d5fa6e62c9823a162dc29500190f
Jiri Hon <jiri.hon@gmail.com>
2023-08-25 09:11:51 +0200

    add missing memorials to pslm files

--- memories_parser.php
+++ memories_parser.php
@@ -14,11 +14,8 @@ define('PSLM_LINE_MARGIN', 2);
 //define('PSLM_ASTERISK_LINE_MARGIN', 10);
 define('PSLM_WORD_MARGIN', 2);
 
-// Really want to override?
-$psalm_lines = pslm_pdf_to_psalm_lines('db/svatky_a_pamatky.xml.gz', 'r');
-//var_dump($psalm_lines);
-//pslm_save_psalm_lines($psalm_lines, 'db/nedelni_zaltar.yml');
-
+// Really want to override? This is supposed to be one-time operation!
+pslm_save_memorials_to_psalms('db/svatky_a_pamatky.xml.gz', 'r');
 
 function pslm_save_psalm_lines($psalm_lines, $filename) {
     file_put_contents($filename, Yaml::dump($psalm_lines));
@@ -92,7 +89,7 @@ function pslm_text_lines_to_psalm_lines($text_lines) {
     return $psalm_lines;
 }
 
-function pslm_pdf_to_psalm_lines($filename) {
+function pslm_save_memorials_to_psalms($filename) {
     $fp = gzopen($filename, 'r');
     //$page = 0;
     $text_lines = [];
@@ -124,6 +121,7 @@ function pslm_pdf_to_psalm_lines($filename) {
     $psalm_lines = array_merge($psalm_lines, pslm_text_lines_to_psalm_lines($text_lines));
 
     $regex = '#^(\d+. \d+\.) (.*?) (\(?(Památka|Svátek|Slavnost)\)? )?(OL\d+)#';
+    $memorials = [];
 
     $fp = fopen('db/svatky_a_pamatky.csv', 'w');
     fputcsv($fp, ['date', 'occasion', 'psalm']);
@@ -134,15 +132,80 @@ function pslm_pdf_to_psalm_lines($filename) {
         if (!$m) {
             preg_match($regex, sprintf('%s %s', $line, $psalm_lines[$i+1]), $m);
             if ($m) {
-                var_dump('combined line');
-                ++$i;
+                $i++; // skip the next line
             }
         }
         if ($m) {
-            var_dump($m);
-            fputcsv($fp, [$m[1], $m[2], $m[5]]);
+            $date = $m[1];
+            $occasion = $m[2];
+            $id = $m[5];
+            $row = [$date, $occasion, $id];
+            $memorials[] = $row;
+            fputcsv($fp, $row);
         }
     }
     fclose($fp);
+
+    $number_to_month = [
+        '1' => 'ledna',
+        '2' => 'února',
+        '3' => 'března',
+        '4' => 'dubna',
+        '5' => 'května',
+        '6' => 'června',
+        '7' => 'července',
+        '8' => 'srpna',
+        '9' => 'září',
+        '10' => 'října',
+        '11' => 'listopadu',
+        '12' => 'prosince',
+    ];
+
+    $solved_psalm_dates = [];
+    $psalm_occasions = json_decode(file_get_contents('db/psalm_occasions.json'));
+
+    foreach ($psalm_occasions as $id => $occasions) {
+        $solved_psalm_dates[$id] = [];
+        foreach ($occasions as $occasion) {
+            preg_match('#\((\d+\. [^ ,\)]+)#', $occasion, $m);
+            if ($m) {
+                $date = $m[1];
+                if (!isset($solved_psalm_dates[$id][$date])) {
+                    $solved_psalm_dates[$id][$date] = true;
+                } else {
+                    echo "WARNING: More than one occasion for $date in psalm $id\n";
+                }
+            }
+        }
+    }
+
+    $psalm_occasions_to_save = [];
+
+    foreach ($memorials as $memorial) {
+        $date = $memorial[0];
+        $occasion = $memorial[1];
+        $id = $memorial[2];
+        preg_match('#^(\d+)\. (\d+)\.$#', $date, $m);
+        if (!$m) {
+            echo "ERROR: No date match\n";
+        }
+        $long_date = sprintf('%s. %s', $m[1], $number_to_month[$m[2]]);
+        
+        if (isset($solved_psalm_dates[$id][$long_date])) {
+            echo "Skipping $date $occasion $id\n";
+            continue;
+        }
+        if (!isset($psalm_occasions_to_save[$id])) {
+            $psalm_occasions_to_save[$id] = [];
+        }
+        $psalm_occasions_to_save[$id][] = sprintf('%%%% occasion: \'%s (%s)\'', $occasion, $long_date);
+    }
+
+    foreach ($psalm_occasions_to_save as $id => $occasions) {
+        $psalm_path = sprintf('pslm/%s.pslm', $id);
+        $psalm = file_get_contents($psalm_path);
+        $new_psalm = str_replace("%% responsum_reference", sprintf("%s\n%%%% responsum_reference", implode("\n", $occasions)), $psalm);
+        file_put_contents($psalm_path, $new_psalm);
+    }
     return $psalm_lines;
 }
